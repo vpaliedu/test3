8d59eff6a4705c4cac8ff8f3f590032a0a38c3af
vpaliedu <118658852+vpaliedu@users.noreply.github.com>
2022-11-23 12:28:21 +0100

    Update game_data.py
    Instagram number modified

--- game_data.py
+++ game_data.py
@@ -1,7 +1,7 @@
 data = [
     {
         'name': 'Instagram',
-        'follower_count': 346,
+        'follower_count': 345,
         'description': 'Social media platform',
         'country': 'United States'
     },
